e2ee45e1a05730821877b007db15b0924d9d1ef9
rahmeto518 <146076184+rahmeto518@users.noreply.github.com>
2026-05-27 13:16:24 +0300

    Create disp_info.sql

---
+++ sql/raw_to_stage/disp_info.sql
@@ -0,0 +1,42 @@
+INSERT INTO stage.disp_info1
+(
+    id,
+    group_cd,
+    ds_cd,
+    mo_cd,
+    in_dt,
+    out_dt,
+    mpioip
+)
+SELECT
+    id,
+    group_cd,
+    ds_cd,
+    mo_cd,
+    in_dt,
+    out_dt,
+    mpioip
+FROM
+(
+    SELECT
+        cmp_id AS id,
+        toInt32(
+            splitByChar('/', diagnosis_group_reference)[2]
+        ) AS group_cd,
+        toInt32(
+            splitByChar('/', diagnosis_reference)[2]
+        ) AS ds_cd,
+        toInt32(
+            splitByChar('/', mo_reference)[2]
+        ) AS mo_cd,
+        period_start AS in_dt,
+        period_end AS out_dt,
+        mpi_oip AS mpioip,
+        row_number() OVER (
+            PARTITION BY id
+            ORDER BY created_at DESC
+        ) AS rn
+
+    FROM raw.event_dispensary_episode
+)
+WHERE rn = 1;
