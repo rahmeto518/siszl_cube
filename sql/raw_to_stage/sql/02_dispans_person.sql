4123a059ce11297afc9c32bf0951818814a6cead
rahmeto518 <146076184+rahmeto518@users.noreply.github.com>
2026-06-15 09:20:37 +0300

    Create 02_dispans_person.sql

---
+++ sql/raw_to_stage/sql/02_dispans_person.sql
@@ -0,0 +1,42 @@
+INSERT INTO stage.dispans_person
+(
+    mpioip,
+    birth_dt,
+    death_dt,
+    pol_cd,
+    attach_mo_cd,
+    ok_region_cd,
+    smo_region_cd,
+    smo_cd
+)
+SELECT
+    mpioip,
+    birth_dt,
+    death_dt,
+    pol_cd,
+    attach_mo_cd,
+    ok_region_cd,
+    smo_region_cd,
+    smo_cd
+FROM
+(
+    SELECT
+        a.mpi_oip AS mpioip,
+        a.birth_date AS birth_dt,
+        a.deceased_date AS death_dt,
+        toInt32(splitByChar('/', a.gender_reference)[2]) AS pol_cd,
+        toInt32(splitByChar('/', b.mo_reference)[2]) AS attach_mo_cd,
+        toInt32(splitByChar('/', c.tfoms_reference)[2]) AS ok_region_cd,
+        toInt32(substring(splitByChar('/', c.smo_reference)[2], 1, 2)) AS smo_region_cd,
+        toInt32(splitByChar('/', c.smo_reference)[2]) AS smo_cd,
+        row_number() OVER (
+            PARTITION BY a.id
+            ORDER BY a.created_at DESC
+        ) AS rn
+    FROM raw.event_insured_person a
+    LEFT JOIN raw.event_mo_attachment b
+        ON a.id = b.insured_person_id
+    LEFT JOIN raw.event_oms_policy c
+        ON a.id = c.insured_person_id
+)
+WHERE rn = 1;
